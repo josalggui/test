0f052985daf350ab523e2f31d1e1c99a95a6ca74
José Miguel Algarín <josalggui@i3m.upv.es>
2024-06-02 17:24:37 +0200

    page code for documentation

--- Sphinx/conf.py
+++ Sphinx/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'a'
 copyright = '2024, a'
 author = 'a'
@@ -14,7 +18,7 @@ release = 'a'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- Sphinx/index.rst
+++ Sphinx/index.rst
@@ -1,15 +1,16 @@
-.. a documentation master file, created by
-   sphinx-quickstart on Sun Jun  2 16:54:07 2024.
+.. MaRCoS GUI documentation master file, created by
+   sphinx-quickstart on Wed Aug 30 16:09:02 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to a's documentation!
-=============================
+Welcome to MaRCoS GUI's documentation!
+======================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   code.rst
 
 
 Indices and tables
