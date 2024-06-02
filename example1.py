f2064d02905edd97c2ab84974aa899e490b97bfb
José Miguel Algarín <josalggui@i3m.upv.es>
2024-06-02 16:42:21 +0200

    Adding functions to document

---
+++ example1.py
@@ -0,0 +1,13 @@
+def example1(arg1, arg2):
+    """
+    This function adds two arguments together.
+
+    Args:
+        arg1: some value
+        arg2: another value
+
+    Returns:
+        arg1 + arg2
+    """
+    arg3 = arg1 + arg2
+    return arg3
